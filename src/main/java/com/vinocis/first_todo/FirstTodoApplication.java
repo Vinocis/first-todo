27acf6f19979139336504682bb92ebe2ba624914
Vinicius <vinicius.alexandre68@gmail.com>
2024-04-03 00:24:37 -0300

    first commit

---
+++ src/main/java/com/vinocis/first_todo/FirstTodoApplication.java
@@ -0,0 +1,13 @@
+package com.vinocis.first_todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstTodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinocis/first_todo/FirstTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinocis.first_todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
