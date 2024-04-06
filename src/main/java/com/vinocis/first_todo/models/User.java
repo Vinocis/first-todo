0633dfaf9ee17449427ed76920ad5e1d86653f51
Vinicius <vinicius.alexandre68@gmail.com>
2024-04-05 23:29:38 -0300

    Create User model

---
+++ src/main/java/com/vinocis/first_todo/models/User.java
@@ -0,0 +1,58 @@
+package com.vinocis.first_todo.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true)
+    private Integer id;
+    @Column(unique = true, length = 100)
+    @Size(min = 2, max = 50)
+    @NotBlank
+    private String username;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Column(unique = true)
+    @Size(min = 8, max = 50)
+    @NotBlank
+    private String password;
+
+    // private List<Task> tasks = new ArrayList<Task>();
+
+    public User(Integer id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
